0b33037965b7a2ea7f3ead15336a489a2b5f3f34
Umm-e-Hani02 <165858620+Umm-e-Hani02@users.noreply.github.com>
2024-10-31 12:39:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -12,29 +12,3 @@ toggleButtonEducation.addEventListener("click", () => {
         toggleButtonEducation.textContent = "Show Education";
     }
 });
-/* ---------------------- Skills ---------------------- */
-const toggleButtonSkills = document.getElementById("toggleSkills");
-const skills = document.getElementById("skillsList");
-toggleButtonSkills.addEventListener("click", () => {
-    if (skills.style.display === "none") {
-        skills.style.display = "block";
-        toggleButtonSkills.textContent = "Hide Skills";
-    }
-    else {
-        skills.style.display = "none";
-        toggleButtonSkills.textContent = "Show Skills";
-    }
-});
-/* ---------------------- Work Experience ---------------------- */
-const toggleButtonWork = document.getElementById("toggleWorkExperience");
-const workExperience = document.getElementById("workExperience");
-toggleButtonWork.addEventListener("click", () => {
-    if (workExperience.style.display === "none") {
-        workExperience.style.display = "block";
-        toggleButtonWork.textContent = "Hide Work Experience";
-    }
-    else {
-        workExperience.style.display = "none";
-        toggleButtonWork.textContent = "Show Work Experience";
-    }
-});
